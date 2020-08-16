ad7d138ede2e94586648b195e464ba895b6ac4ed
Mikhail Chepelev <michaelchep@yandex.ru>
2020-08-16 18:10:37 +0300

    delete befor buil

--- src/utils/utils.js
+++
@@ -1,5 +0,0 @@
-export const defaultUrl = 'https://mesto.nomoreparties.co';
-export const myToken = '072afc53-82e4-4aa4-b920-dd02ed4506a2';
-export const myGroup = 'cohort-13';
-export const defaultType = 'application/json';
-
