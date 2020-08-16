a14c4d8d8de82dd62fdc4255258a416459094e83
Mikhail Chepelev <michaelchep@yandex.ru>
2020-08-16 18:15:34 +0300

    Revert "delete befor buil"
    This reverts commit ad7d138ede2e94586648b195e464ba895b6ac4ed.

---
+++ src/components/PopupWithForm.js
@@ -0,0 +1,23 @@
+import React from 'react';
+
+function popupWithForm(props) {
+  return (
+    <div className={`popup popup_type_${props.name} ${props.isOpen ? 'popup_opened' : ''}`}>
+      <div className="popup__container">
+        <button
+          className="popup__close-button"
+          type="button"
+          aria-label="Закрыть окно"
+          onClick={props.onClose}
+        ></button>
+        <form className={`form form_type_${props.name}`} name={props.name} action="#" method="GET" noValidate>
+          <h3 className="form__title">{props.title}</h3>
+          {props.children}
+          <button className="form__submit-button" type="submit">{props.buttonName}</button>
+        </form>
+      </div>
+    </div>
+  );
+}
+
+export default popupWithForm;
